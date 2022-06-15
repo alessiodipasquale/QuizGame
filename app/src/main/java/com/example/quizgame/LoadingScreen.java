c32ec33d59012947ce2aaa43c2f06e3aeb9c71f4
Alessio <alessiodipasquale2000@gmail.com>
2022-06-15 22:25:32 +0200

    Started Game Page

--- app/src/main/java/com/example/quizgame/LoadingScreen.java
+++ app/src/main/java/com/example/quizgame/LoadingScreen.java
@@ -2,7 +2,11 @@ package com.example.quizgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class LoadingScreen extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class LoadingScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+        GifImageView gif = findViewById(R.id.gifLoading);
+        gif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoadingScreen.this, GameScreen.class));
+            }
+        });
     }
 }
